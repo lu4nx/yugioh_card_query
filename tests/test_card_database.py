75bafd5c83312bbcb8afbd1832332edade1e74ae
lu4nx <lx@shellcodes.org>
2020-12-08 21:16:39 +0800

    重构单元测试

--- tests/test_card_database.py
+++ tests/test_card_database.py
@@ -8,35 +8,35 @@ class TestCardBase(TestCase):
 
     def test_normal_monster(self):
         self.assertEqual(self.base.get_card_type(0x11),
-                         "怪兽")
+                         "怪兽/通常")
 
     def test_link_monster(self):
         self.assertEqual(self.base.get_card_type(0x4000021),
-                         "怪兽 连接 效果")
+                         "怪兽/连接/效果")
 
     def test_fusion_monster(self):
         self.assertEqual(self.base.get_card_type(0x41),
-                         "怪兽 融合")
+                         "怪兽/融合")
 
     def test_is_pendulum_monster(self):
         self.assertEqual(self.base.get_card_type(0x1000021),
-                         "怪兽 灵摆 效果")
+                         "怪兽/灵摆/效果")
 
     def test_is_ritual_monster(self):
         self.assertEqual(self.base.get_card_type(0x81),
-                         "怪兽 仪式")
+                         "怪兽/仪式")
 
     def test_is_xyz_monster(self):
         self.assertEqual(self.base.get_card_type(0x800021),
-                         "怪兽 超量 效果")
+                         "怪兽/超量/效果")
 
     def test_is_synchro_monster(self):
         self.assertEqual(self.base.get_card_type(0x2021),
-                         "怪兽 同调 效果")
+                         "怪兽/同调/效果")
 
     def test_effect_monster(self):
         self.assertEqual(self.base.get_card_type(0x2000021),
-                         "怪兽 效果")
+                         "怪兽/特殊召唤/效果")
 
     def test_normal_spell(self):
         self.assertEqual(self.base.get_card_type(0x2),
@@ -44,23 +44,23 @@ class TestCardBase(TestCase):
 
     def test_equip_spell(self):
         self.assertEqual(self.base.get_card_type(0x40002),
-                         "魔法 装备")
+                         "魔法/装备")
 
     def test_quick_play_spell(self):
         self.assertEqual(self.base.get_card_type(0x10002),
-                         "魔法 速攻")
+                         "魔法/速攻")
 
     def test_ritual_spell(self):
         self.assertEqual(self.base.get_card_type(0x82),
-                         "魔法 仪式")
+                         "魔法/仪式")
 
     def test_continuous_spell(self):
         self.assertEqual(self.base.get_card_type(0x20002),
-                         "魔法 永久")
+                         "魔法/永久")
 
     def test_field_spell(self):
         self.assertEqual(self.base.get_card_type(0x80002),
-                         "魔法 场地")
+                         "魔法/场地")
 
     def test_normal_trap(self):
         self.assertEqual(self.base.get_card_type(0x4),
@@ -68,11 +68,11 @@ class TestCardBase(TestCase):
 
     def test_continuous_trap(self):
         self.assertEqual(self.base.get_card_type(0x20004),
-                         "陷阱 永久")
+                         "陷阱/永久")
 
     def test_counter_trap(self):
         self.assertEqual(self.base.get_card_type(0x100004),
-                         "陷阱 反击")
+                         "陷阱/反击")
 
     def test_get_attribute(self):
         self.assertEqual(self.base.get_card_attribute(0x1), "地")
